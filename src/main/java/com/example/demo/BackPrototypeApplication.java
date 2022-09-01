a128a54556879f978b7853f0144982ea67a5ff24
Goder-0 <goder0@naver.com>
2022-09-01 18:40:22 +0900

    initial commit

---
+++ src/main/java/com/example/demo/BackPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPrototypeApplication.class, args);
+	}
+
+}
