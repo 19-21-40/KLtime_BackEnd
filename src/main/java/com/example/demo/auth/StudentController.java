7590bd713afed18940e26ba50311ee916bc4c9f9
Goder-0 <goder0@naver.com>
2022-10-13 18:58:56 +0900

    Login SignUp update

--- src/main/java/com/example/demo/auth/StudentController.java
+++ src/main/java/com/example/demo/auth/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.demo.auth;
 
+import com.example.demo.Repository.DepartmentRepository;
+import com.example.demo.domain.Department;
 import com.example.demo.domain.Student;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.StudentDTO;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.NoResultException;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class StudentController {
     private final StudentService studentService;
-
+    private final DepartmentRepository departmentRepository;
     private final TokenProvider tokenProvider;
 
     private final PasswordEncoder passwordEncoder =new BCryptPasswordEncoder();
@@ -25,18 +29,22 @@ public class StudentController {
     @PostMapping("/sign_up")
     public ResponseEntity<?> registerStudent(@RequestBody StudentDTO studentDTO) {
         try {
-            Student student=Student.from(studentDTO).orElseThrow(IllegalArgumentException::new);
+            Student student=Student.from(studentDTO)
+                    .orElseThrow(()->{throw new IllegalStateException("지정된 형식과 일치하지 않습니다.");});
             student.setPassword(passwordEncoder.encode(studentDTO.getPassword()));
-
+            Department department=departmentRepository.findByName(studentDTO.getDepartmentName())
+                    .orElseThrow(()->{throw new IllegalStateException("해당학부가 존재하지 않습니다.");});
+            student.setDepartment(department);
             Student registeredStudent = studentService.create(student);
             StudentDTO responseStudentDTO = StudentDTO.builder()
+                    .grade(studentDTO.getGrade())
                     .number(registeredStudent.getNumber())
                     .name(registeredStudent.getName())
                     .email(registeredStudent.getEmail())
                     .build();
-            System.out.println(registeredStudent);
             return ResponseEntity.ok().body(responseStudentDTO);
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             ResponseDTO<Object> responseDTO = ResponseDTO.builder()
                     .error(e.getMessage())
                     .build();
@@ -44,7 +52,7 @@ public class StudentController {
         }
     }
 
-    @PostMapping("/sign_in/")
+    @PostMapping("/sign_in")
     public ResponseEntity<?> authenticate(@RequestBody StudentDTO studentDTO){
         Student student = studentService.getByCredentials(
                 studentDTO.getNumber(),
